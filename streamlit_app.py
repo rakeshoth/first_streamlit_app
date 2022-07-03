0a164b1e9710fca3bcf4ec7289f7ceb813334164
Githib Rak <rakesh.oth@gmail.com>
2022-07-02 17:34:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 import pandas as pd
 
-streamlit.title('My Mom''s New Healthy Diner')
+streamlit.title('My Mom''''s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
