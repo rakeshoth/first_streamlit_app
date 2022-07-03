b71e1c81e218f808b9696137366d93b9254a18bd
Githib Rak <rakesh.oth@gmail.com>
2022-07-02 17:12:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Har-Boiled Free-Range Egg')
+
+print('myapp')
