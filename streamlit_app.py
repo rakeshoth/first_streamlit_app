5ff2630e9079af9697c85b0df9ec8497e606fda7
Githib Rak <rakesh.oth@gmail.com>
2022-07-08 13:54:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@
 import streamlit
 import pandas as pd
 import requests
-
+import snowflake.connector
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Har-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avacado Toasts')
 
 
 # Display the table on the page.
