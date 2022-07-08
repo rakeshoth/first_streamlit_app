27574075322f657bda25da6486b8418ac214c41d
Githib Rak <rakesh.oth@gmail.com>
2022-07-08 14:09:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,10 @@ fruitvice_requests = requests.get("https://fruityvice.com/api/fruit/" + fruit_ch
 fruitvice_normalized = pd.json_normalize(fruitvice_requests.json())
 streamlit.dataframe(fruitvice_normalized)
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
